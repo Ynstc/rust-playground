be3d5b603abf6c9d1bb4a631aa0dd26eb2ac5b1f
Ernest Kost <ernest.kost@playbookengineering.com>
2022-05-18 01:14:10 +0200

    reference counting

--- lgr22_box_smart_pointer/src/main.rs
+++ lgr22_box_smart_pointer/src/main.rs
@@ -17,6 +17,7 @@ fn main() {
     box_pointer();
     drop_trait_order();
     drop_trait_manually();
+    reference_counting_print();
 }
 
 /* 22.0.1 The box smart pointer */
@@ -113,4 +114,51 @@ fn drop_trait_manually() {
     drop(c); //different method than ours. Rust standard library
     println!("CustomerSmartPointer dropped before the end of main.")
 
+}
+
+/* 23.0.0 reference counting */
+enum List2 {
+    Cons2(i32, Box<List2>),
+    Nil2,
+}
+
+use List2::{Cons2, Nil2};
+
+fn reference_counting() {
+    let a = Cons2(5, Box::new(Cons2(10, Box::new(Nil2))));
+    let b = Cons2(3, Box::new(a));
+    // let c = Cons2(4, Box::new(a)); //hover over erron on (a) - has been moved
+}
+
+//goes to:
+
+use std::rc::Rc;
+
+enum List3 {
+    Cons3(i32, Rc<List3>),
+    Nil3
+}
+
+use crate::List3::{Cons3,Nil3};
+
+fn reference_counting_refactored(){
+    let a = Rc::new(Cons3(5, Rc::new(Cons3(10, Rc::new(Nil3)))));
+    let b = Cons3(3, a.clone()); //this and
+    let c = Cons3(4, Rc::clone(&a)); //that - both works the same
+}
+
+/* 23.0.1 example of refererence counting */
+
+fn reference_counting_print(){
+    let a = Rc::new(Cons3(5, Rc::new(Cons3(10, Rc::new(Nil3)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+
+    let b = Rc::new(Cons3(3, Rc::clone(&a)));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+
+    {
+        let c = Rc::new(Cons3(4, Rc::clone(&a)));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
