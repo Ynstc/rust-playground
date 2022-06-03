5aeb41a87c266b8ae6a59f33d341511690920139
Ynstc <koynst@gmail.com>
2022-06-03 23:57:56 +0200

    trait objects

---
+++ lgr32_trait_objects/src/lib.rs
@@ -0,0 +1,55 @@
+/* 32.0.0 Trait objects */
+// more flexibility but has performance cost
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+//why not use generic instead of trait object case: (wrong)
+// look there is T which will set one type of data,
+// so you cannot store mix of data (e.g buttons input fields etc.)
+// but homogenous type (only buttons or only input fields etc.)
+// if only one type then this solution with generics are preferable because of runtime cost
+
+// pub struct Screen<T: Draw> {
+//     pub components: Vec<T>,
+// }
+
+// impl<T> Screen<T>
+//     where T: Draw,
+// {
+//     pub fn run(&self) {
+//         for component in self.components.iter() {
+//             component.draw();
+//         }
+//     }
+// }
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // draw button
+    }
+}
+
+//'object safe' traits rules:
+// all methods on that trait:
+// 1. return type is not 'self'
+// 2. are no generic parameters
---
+++ lgr32_trait_objects/src/main.rs
@@ -0,0 +1,36 @@
+use lgr32_trait_objects::{Screen, Button, Draw};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // draw select box
+    }
+}
+fn main() {
+    let screen = Screen {
+        components: vec![
+            // Box::new(String::from("some string - check error")),
+            Box::new(SelectBox {
+                width: 100,
+                height: 100,
+                options: vec![
+                    String::from("yes"),
+                    String::from("no"),
+                    String::from("maybe")
+                ]
+            }),
+            Box::new(Button {
+                width: 100,
+                height: 100,
+                label: String::from("ok"),
+            })
+        ],
+    };
+
+    screen.run();
+}
